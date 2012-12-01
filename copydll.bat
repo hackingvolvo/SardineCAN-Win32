a0982807a02e3f5aa431583baa5619e4ae8e8b36
Olaf <hackingvolvo@gmail.com>
2012-12-01 15:06:04 +0200

    b

--- copydll.bat
+++ copydll.bat
@@ -2,6 +2,6 @@ cd "c:\users\olento\Documents\Visual Studio 2010\Projects\Sardine"
 rem copy Debug\sardine.dll c:\windows\system32\sardine.dll
 rem copy Debug\sardine.dll c:\windows\syswow64\sardine.dll
 copy Debug\sardine.dll c:\sardine\sardine.dll
-copy debug\sardinetest.exe c:\sardine\sardincfg.exe
+copy debug\sardinetest.exe c:\sardine\sardinetest.exe 
 copy Release\sardine.dll c:\vm-share\sardine.dll
-copy Release\sardinetest.exe c:\vm-share\sardinecfg.exe
+copy Release\sardinetest.exe c:\vm-share\sardinetest.exe 
